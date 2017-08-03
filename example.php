19f068f093afe7c3b487e4e9d68099a265094d6d
Reza Juliandri <rezajuliandri20@yahoo.com>
2017-08-03 21:13:59 +0700

    Update example.php

--- example.php
+++ example.php
@@ -1,10 +1,10 @@
 <?php
 define("BASEPATH",__DIR__);
-//header('Content-Type: application/json');
+header('Content-Type: application/json');
 require_once('vendor/autoload.php');
 use Other\Development;
-$Development = new Development;
-$Development::timer();
+//$Development = new Development;
+//$Development::timer();
 const __HOST__ = "YOUR HOST";
 const __USER__ = "YOUR USERNAME";
 const __PASS__ = "YOUR PASSWORD";
